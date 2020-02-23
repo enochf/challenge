ba2b7469b83014a20f7e104a0a17e09932689d71
enochf <enoch@enochfredericks.com>
2020-02-22 22:00:50 -0700

    Update main.js
    0:45 - added buttons and more explanation of how functions work and interact

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 //      Elevator class
 //      Building
 //      Controller
+//      Buttons
 
 // building object sets default values for the rest of the challenge
 var building = {
@@ -18,26 +19,41 @@ class Elevator {
         this.log = []; // houses a array of objects which include timestamped records of trips, and floors passed
         this.inService = true; // indicates whether or not an elevator is in service
     }
-    answerCall() {
+    answerDispatch() {
         // answer the call for an elevator
     }
     report() {
         // reports when elevator opens and closes door
     }
-    tracking() {
+}
 
+// method by which elevators are called
+class button {
+    constructor() {
+        this.floor = null; // indecates which floor the button is assigned to
+        this.pushed = false; // indicates if the button has been pushed and is active or if it is inactive
+    }
+    push() {
+        // calls the closest elecator and sets pushed status to true
+    }
+    reset() {
+        // resets button to inactive mode
     }
 }
 
+// controls the calling of elevators
 var controller {
-    init: function(floors, elevators) {
+    init: function() {
         // initiates the elevator simulation
+        // 1. runs a for loop with the amount of elevators indicated in building.elevators
+        // 2. 
     },
     elevators: [], // houses all of the elevator objects once they're created by the init() function and they're current position
     findElevator: function() {
         // searches for the closest elevator based on their position, movement and service level
+        // ends with this.dispatch(elev); to call the correct elevator
     },
-    call: function() {
-        // calls the designated elevator to the call floor
+    dispatch: function() {
+        // dispatches the designated elevator to the call floor
     }
 }
