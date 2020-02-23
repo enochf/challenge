71a8384cfc2bbb1d4e6a8b788a62ca54e573bf23
enochf <enoch@enochfredericks.com>
2020-02-22 21:47:31 -0700

    Update main.js
    0:30 - added login to controller and accounted for service level

--- main.js
+++ main.js
@@ -15,11 +15,29 @@ class Elevator {
         this.curFloor = 1; // tells which floor the elevator is currntly on
         this.moving = false; // indicatates whether or not the elevator is in motion or stopped
         this.direction = null; // indicates the direction (up or down) the elevator is moving
+        this.log = []; // houses a array of objects which include timestamped records of trips, and floors passed
+        this.inService = true; // indicates whether or not an elevator is in service
+    }
+    answerCall() {
+        // answer the call for an elevator
     }
     report() {
         // reports when elevator opens and closes door
     }
-    answerCall() {
-        // answer the call for an elevator
+    tracking() {
+
+    }
+}
+
+var controller {
+    init: function(floors, elevators) {
+        // initiates the elevator simulation
+    },
+    elevators: [], // houses all of the elevator objects once they're created by the init() function and they're current position
+    findElevator: function() {
+        // searches for the closest elevator based on their position, movement and service level
+    },
+    call: function() {
+        // calls the designated elevator to the call floor
     }
 }
