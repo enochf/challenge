e39579728cef5dcddd4536b146ef85cffc823a92
enochf <enoch@enochfredericks.com>
2020-02-22 22:17:00 -0700

    Update main.js
    1:00 - added more functionality and detailed explanation of function calls

--- main.js
+++ main.js
@@ -22,6 +22,15 @@ class Elevator {
     answerDispatch() {
         // answer the call for an elevator
     }
+    openDoor() {
+        // opens the door
+    }
+    closeDoor() {
+        // closes the door
+    }
+    floorRequest() {
+        // indicates which floor the elevator need to go to once the door closes
+    }
     report() {
         // reports when elevator opens and closes door
     }
@@ -33,7 +42,7 @@ class button {
         this.floor = null; // indecates which floor the button is assigned to
         this.pushed = false; // indicates if the button has been pushed and is active or if it is inactive
     }
-    push() {
+    pushButton() {
         // calls the closest elecator and sets pushed status to true
     }
     reset() {
@@ -46,9 +55,19 @@ var controller {
     init: function() {
         // initiates the elevator simulation
         // 1. runs a for loop with the amount of elevators indicated in building.elevators
-        // 2. 
+        // 2. craetes a new Elveator object for each loop and pushes it to the controller.elevators[] array
+        // 3. creates a new button object for each loop
+        // 4. calls the controller.randomCaller() function to call the first elevator
     },
-    elevators: [], // houses all of the elevator objects once they're created by the init() function and they're current position
+    elevators: [], // houses all of the elevator objects once they're created by the controller.init() function and they're current position
+    buttons: [], // houses all of the button objects once they're created by the controller.init() 
+    randomCaller: function() {
+        // finds a random floor based on the number of floors indicated the building.foors object
+        // 1. finds a random number between 1 and (building.floors)
+        // 2. calls the elevator using controller.buttons[(random number)].pushButton() function
+        // 3. generates a random time between 1 and 10 seconds
+        // 4. calls itself at this.randomCaller() in order to call another elevator and continue the simulation
+    },
     findElevator: function() {
         // searches for the closest elevator based on their position, movement and service level
         // ends with this.dispatch(elev); to call the correct elevator
